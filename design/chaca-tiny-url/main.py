e946f1792adedbcf20155782955636c00f4b28e6
dbahena <dario.mx@gmail.com>
2017-08-03 19:27:12 -0500

    backup

--- design/chaca-tiny-url/main.py
+++ design/chaca-tiny-url/main.py
@@ -4,7 +4,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, Frickin World!')
+        self.response.write('I will prevail Google!')
 
 
 app = webapp2.WSGIApplication([
