b32de2afd7db958f398a3d60579caf7ea53e61f9
dbahena <dario.mx@gmail.com>
2017-11-23 13:10:20 -0600

    backup

---
+++ leetcode/g/minimum-height-trees/Solution.py
@@ -0,0 +1,57 @@
+"""
+Here is one of the solutions found in literature:
+
+0) Pick arbitrary starting node
+
+1) Do a BFS/DFS search (it does not matter as trees have unique paths), keeping
+   track of the farthest node (any will make the trick, as there could be several
+   with same distance from starting node)
+
+2) Repeat 1) but starting from the farthest node found. The path from this new
+   starting point ands its new farthest node will be the diameter of the tree.
+
+3) There is a theorem saying that center lies at the middle of the diameter,
+   seen as a path. If such path has odd length, there are two centers.
+
+"""
+from collections import defaultdict
+
+class Solution(object):
+    def search_farthest(self, tree, start):
+        stack = [(start, 0)]
+        parent = dict()
+        visited = set()
+        max_dist = 0
+        while stack:
+            node, dist = stack.pop()
+            if node in visited:
+                continue
+            visited.add(node)
+            if dist > max_dist:
+                max_dist = dist
+                farthest = node
+            for child in tree[node]:
+                if child not in parent:
+                    parent[child] = node
+                stack.append((child, dist+1))
+        return max_dist, farthest, parent
+
+    def findMinHeightTrees(self, n, edges):
+        if not edges:
+            return [0] * n
+        tree = defaultdict(lambda: [])
+        for u, v in edges:
+            tree[u].append(v)
+            tree[v].append(u)
+        start = tree.iterkeys().next()
+        _, farthest1, _ = self.search_farthest(tree, start)
+        max_dist, farthest2, parent = self.search_farthest(tree, farthest1)
+        node = farthest2
+        for _ in xrange(max_dist/2):
+            node = parent[node]
+        roots = [node]
+        if max_dist % 2 == 1:
+            roots.append(parent[node])
+        return roots
+
+    
---
+++ leetcode/g/ones-and-zeroes/Solution.py
@@ -0,0 +1,64 @@
+"""
+First of all, I am not really sure this is a generic solution. In general, we may
+need to do some sort of backtracking or DP. I refused to do that, making the theory that this could be solved with a greedy algorithm. It seems to work for the test cases, but it may not work for all cases.
+
+Alright, warnings said, let us describe the solution. It is composed of two stragegies combined (which is sort of the order in which I conceived them):
+
+0. Count how many zeros and ones we have for each string (pre-processing).
+
+1. If this is a greedy algorithm, then there must be an order which tells us which strings to take first. There seem to be actually two orders, given by following sort key functions:
+
+1.1 minimum(# of zeros left to use, # of ones left to use), reverse order
+1.2 minimum(# of zeros in string, # of ones in string), regular order
+
+The first strategy favors those strings leaving more 0's and 1's available, while the second prefers those consuming less 0's and 1's. Under difference circumstances (tests), one order seems to work better than the other. So we should consider both.
+
+2. For each order, we traverse the sorted sequence of strings checking if they meet the constraints. Those passing the checks are counted.
+
+3. We return the maximum counter from the two orders tried.
+
+"""
+
+from collections import defaultdict
+
+
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        ones = defaultdict(lambda: 0)
+        zeros = defaultdict(lambda: 0)
+        k = len(strs)
+        for i in xrange(k):
+            s = strs[i]
+            for c in s:
+                if c == '0':
+                    zeros[i] += 1
+                else:
+                    ones[i] += 1
+
+        def s_key1(i):
+            zeros_left = m - zeros[i]
+            ones_left = n - ones[i]
+            return min(zeros_left, ones_left)
+
+        def s_key2(i):
+            return min(zeros[i], ones[i])
+
+        def try_order(sorted_idx):
+            used = 0
+            m_cnt = m
+            n_cnt = n
+            for i in sorted_idx:
+                s = strs[i]
+                if zeros[i] <= m_cnt and ones[i] <= n_cnt:
+                    m_cnt -= zeros[i]
+                    n_cnt -= ones[i]
+                    used += 1
+            return used
+
+        # main
+        sorted_idx = sorted(xrange(k), key=s_key1, reverse=True)
+        used1 = try_order(sorted_idx)
+        sorted_idx = sorted(xrange(k), key=s_key2, reverse=False)
+        used2 = try_order(sorted_idx)
+        return max(used1, used2)
+
