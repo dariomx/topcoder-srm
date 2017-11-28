93a88111aae66133a25db29538939815a49b996b
dbahena <dario.mx@gmail.com>
2017-11-28 12:40:24 -0600

    backup

---
+++ leetcode/g/sentence-similarity-ii/Solution.py
@@ -0,0 +1,61 @@
+"""
+The description of the exercise gives us already a lot of hits, as they describe the properties of the being-similar relation for words:
+
+1. Reflexive
+2. Symmetric
+3. Transitive
+
+Thus, the relation is pretty much an equivalence relation; though that may not help us much for the exercise (or it might be, as the relation brings a natural partition to the strings set; but we did not use that here).
+
+The approach is simple: build a graph from the pairs, using a searchable collection of children for each node (string); a set was good enough. Then validate each property of the relation as an alternative for having similar words.
+
+The first two properties are directly validated from the words themselves, or the children sets of the graph; and for the transitivity property we just validate for reachability in the graph (using DFS, though BFS would serve too).
+
+Last but not least, we just use the hint and validate upfront if the lenghts of the words are same or not. In all functions indeed, we try to leave as soon as possible when similarity is not achieved.
+
+"""
+
+from collections import defaultdict
+from itertools import izip
+
+
+class Solution(object):
+    def create_graph(self, pairs):
+        graph = defaultdict(lambda: set())
+        for x, y in pairs:
+            graph[x].add(y)
+            graph[y].add(x)
+        return graph
+
+    def are_reachable(self, graph, x, y):
+        stack = [x]
+        visited = set()
+        while stack:
+            node = stack.pop()
+            if node in visited:
+                continue
+            visited.add(node)
+            if node == y:
+                return True
+            for child in graph[node]:
+                stack.append(child)
+        return False
+
+    def are_similar(self, w1, w2, graph):
+        if w1 == w2:  # reflexivity
+            return True
+        elif w1 in graph[w2] or w2 in graph[w1]:  # symmetry
+            return True
+        elif self.are_reachable(graph, w1, w2):  # transitivity
+            return True
+        else:
+            return False
+
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
+        if len(words1) != len(words2):
+            return False
+        graph = self.create_graph(pairs)
+        for w1, w2 in izip(words1, words2):
+            if not self.are_similar(w1, w2, graph):
+                return False
+        return True
---
+++ leetcode/g/sentence-similarity-ii/Solution1.py
@@ -0,0 +1,77 @@
+"""
+While the previous DFS approach works, it can potentially traverse long sections of the graph without a real need. In order to improve further, we can leverage the previously ignored properties of the similar relation: it is an equivalence relation.
+
+Equivalence relations bring a natural partition to the set, where in our particular case each partition contains strings similar to each other. If we model each partition as a disjoin-set, then we could optimize the transitivity check: if two strings belong to same disjoint set, then they are similar.
+
+Knowing two strings belong to same disjoint-set, means retrieving the representative for each one, and comparing those representatives. Retrieving each representative, in a disjoint-set forests implementation, can be done in O(log(n)). This should be better than the linear complexity from DFS.
+
+The disjoint-set forest was taken from Cormen book.
+
+NOTE: Despite theoretical advantages mentioned, this algorithm actually is slower than the DFS one. Perhaps the paths to parent are too long (unbalanced trees), or perhaps the path-compression is worth it only if you query more times.
+
+"""
+
+from itertools import izip
+
+
+class DisjointSet:
+    def __init__(self, val):
+        self.p = self
+        self.rank = 0
+
+
+def make_set(x):
+    return DisjointSet(x)
+
+
+def union(x, y):
+    link(find_set(x), find_set(y))
+
+
+def link(x, y):
+    if x.rank > y.rank:
+        y.p = x
+    else:
+        x.p = y
+        if x.rank == y.rank:
+            y.rank += 1
+
+
+def find_set(x):
+    if x != x.p:
+        x.p = find_set(x.p)
+    return x.p
+
+
+class Solution(object):
+    def create_forest(self, pairs):
+        forest = dict()
+        for x, y in pairs:
+            if x not in forest:
+                forest[x] = make_set(x)
+            if y not in forest:
+                forest[y] = make_set(y)
+            union(forest[x], forest[y])
+        return forest
+
+    def are_similar(self, w1, w2, forest):
+        # reflexivity
+        if w1 == w2:
+            return True
+        # they can't be similar if one is not in forest
+        elif w1 not in forest or w2 not in forest:
+            return False
+            # symmetry & transitivity
+        elif find_set(forest[w1]) == find_set(forest[w2]):
+            return True
+        else:
+            return False
+
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
+        if len(words1) != len(words2):
+            return False
+        forest = self.create_forest(pairs)
+        for w1, w2 in izip(words1, words2):
+            if not self.are_similar(w1, w2, forest):
+                return False
+        return True
