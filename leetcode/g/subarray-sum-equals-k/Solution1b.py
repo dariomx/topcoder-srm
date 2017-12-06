a89b1bebb1cede6d4b39c6d74ed362e678424501
dbahena <dario.mx@gmail.com>
2017-12-06 00:36:58 -0600

    backup

--- leetcode/g/subarray-sum-equals-k/Solution1b.py
+++ leetcode/g/subarray-sum-equals-k/Solution1b.py
@@ -1,3 +1,16 @@
+"""
+My Python solution with prefix sums: it explores all the O(n^2) subarrays,
+but evaluates each subarray [i,j] efficiently in O(1) time with the formula
+psum[j] - psum[i]. Sadly, this times out due the language chosen.
+
+See the Java and C ports of the same algorithm (on this same directory),
+they get both accepted. Interestingly the Java version runs twice as fast as
+the C version! I never thought that I would see the say when C was
+outperformed by Java ;-?
+
+"""
+
+
 class Solution(object):
     def calc_psum(self, nums, n):
         psum = [0] * n
@@ -14,6 +27,6 @@ class Solution(object):
         cnt = 0
         for i in xrange(n):
             for j in xrange(i, n):
-                if psum[j] - (psum[i-1] if i > 0 else 0) == k:
+                if psum[j] - (psum[i - 1] if i > 0 else 0) == k:
                     cnt += 1
         return cnt
