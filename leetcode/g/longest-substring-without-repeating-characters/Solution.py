4788ebd1175af6af76f0aaaa8e6f00183beb4293
dbahena <dario.mx@gmail.com>
2017-12-06 12:38:22 -0600

    backup

--- leetcode/g/longest-substring-without-repeating-characters/Solution.py
+++ leetcode/g/longest-substring-without-repeating-characters/Solution.py
@@ -9,12 +9,13 @@ We keep track of the start/end indexes of our sliding window, which represent
 the current substring to explore. In order to detect duplication, we keep
 track of the last index seen for each character. If the current character
 was seen before start index, that means it lies out of our window and we do not
-care. If it lies within, we need to shift our start index to one position
-after the last seen index.
+care; we can proceed to compute the window's length and update our maximum
+accordingly. But if last index lies within our current window, we need to
+shift our start index to one position after it.
 
 It may look dramatic to discard all characters occurring prior last seen
 index, given that the only offending one was current character. But since the
-substring needs to be a continuous section, a guy in the middle can actually
+substring needs to be a contiguous section, a guy in the middle can actually
 spoil big areas. In this case, the whole left portion of the current window
 (left to the last seen occurrence of character).
 
@@ -28,10 +29,9 @@ class Solution(object):
         max_len = 0
         for end in xrange(len(s)):
             c = s[end]
-            curr_len = end - start + 1
             c_idx, last_idx[c] = last_idx.get(c, -1), end
             if c_idx < start:
-                max_len = max(max_len, curr_len)
+                max_len = max(max_len, end - start + 1)
             else:
                 start = c_idx + 1
         return max_len
