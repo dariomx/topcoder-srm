7699ee118cbfd2347abc48d52e4f4375e1594979
dbahena <dario.mx@gmail.com>
2017-12-14 09:51:34 -0600

    backup

---
+++ leetcode/amazon/merge-two-binary-trees/Solution.py
@@ -0,0 +1,23 @@
+"""
+Not really much to tell about; simple recursive solution.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        if t1 and t2:
+            root = TreeNode(t1.val + t2.val)
+            root.left = self.mergeTrees(t1.left, t2.left)
+            root.right = self.mergeTrees(t1.right, t2.right)
+            return root
+        elif t1:
+            return t1
+        else:
+            return t2
