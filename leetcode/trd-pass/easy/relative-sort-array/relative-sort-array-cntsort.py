a87fa293fe6e5009bcd256b9fbf2320fdb844246
dario_bahena <dario.bahena@oracle.com>
2020-08-30 11:20:02 -0500

    fix relative-sort-array (max_x)

--- leetcode/trd-pass/easy/relative-sort-array/relative-sort-array-cntsort.py
+++ leetcode/trd-pass/easy/relative-sort-array/relative-sort-array-cntsort.py
@@ -1,19 +1,22 @@
 # count-sort based soln, based on another's port seen in phorum (mine used
 # regular sorts)
 
+from math import inf
+
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         n = 1001
         cnt1 = [0] * n
         in_arr2 = [False] * n
+        max_x = -inf
         ans = []
         for x in arr1:
             cnt1[x] += 1
-            n = max(n, x)
+            max_x = max(max_x, x)
         for x in arr2:
             in_arr2[x] = True
             ans += [x] * cnt1[x]
-        for x in range(n):
+        for x in range(max_x+1):
             if not in_arr2[x]:
                 ans += [x] * cnt1[x]
         return ans
